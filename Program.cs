122aaf311d64430e3056ce864847333becec6873
sefademirkaya06 <115144360+sefademirkaya06@users.noreply.github.com>
2022-12-25 12:33:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[,] sayilar = new int[4, 3];
+            //Random rnd = new Random();
+
+            //for (int i = 0; i < sayilar.GetLength(0); i++)
+            //{
+
+            //    for (int j = 0; j < sayilar.GetLength(1); j++)
+            //    {
+
+            //        sayilar[i, j] = rnd.Next(10, 100);
+            //    }
+            //}
+            //for (int i = 0; i < sayilar.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < sayilar.GetLength(1); j++)
+            //    {         
+            //        Console.WriteLine(sayilar[i,j] + " ");
+            //    }
+            //    Console.WriteLine();               
+            //}
+
+            //Console.WriteLine("Kaç adet öğrenci kaydetmek istiyorsunuz? ");
+            //int adet = int.Parse(Console.ReadLine());
+            //string[,] parametreler = new string[adet + 1, 3];
+            //parametreler[0, 0] = "AD";
+            //parametreler[1, 0] = "SOYAD";
+            //parametreler[2, 0] = "NUMARA";
+            //for (int i = 1; i < parametreler.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < parametreler.GetLength(1); j++)
+            //    {
+            //        if (j == 0)
+            //        {
+            //            Console.WriteLine($"{i}.Öğrencinin ismini giriniz");
+            //            parametreler[i, j] = Console.ReadLine();
+            //        }
+            //        else if (j == 1)
+            //        {
+            //            Console.WriteLine($"{i}.Öğrencinin soyadını giriniz:");
+            //            parametreler[i, j] = Console.ReadLine();
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine($"{i}.Öğrencinin numarasını giriniz:");
+            //            parametreler[i, j] = Console.ReadLine();
+            //        }
+
+            //    }
+
+            //}
+            //for (int i = 0; i < parametreler.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < parametreler.GetLength(1); j++)
+            //    {
+            //        Console.Write(parametreler[i,j]+ " ");
+            //    }
+            //    Console.WriteLine();
+
+            //}
+
+
+            Console.WriteLine("Kaç adet öğrenci kaydetmek istiyorsunuz?");
+            int adet = int.Parse(Console.ReadLine());
+            string[,] liste = new string[adet + 1, 3];
+            liste[0, 0] = "Ad";
+            liste[0, 1] = "Soyad";
+            liste[0, 2] = "Numara";
+            for (int i = 1; i < liste.GetLength(0); i++)
+            {
+                for (int j = 0; j < liste.GetLength(1); j++)
+                {
+                    switch(j)
+                    {
+                        case 0: Console.WriteLine($"{i}.Öğrencinin ismini giriniz");
+                            liste[i,j]=Console.ReadLine();
+                            break;
+                        case 1: Console.WriteLine($"{i}. Öğrencinin soyadını giriniz");
+                            liste[i, j] = Console.ReadLine();
+                            break;
+                        case 2: Console.WriteLine($"{i}.Öğrencinin nuamrasını giriniz");
+                                liste[i,j]=Console.ReadLine();
+                            break;
+                    }
+                }
+            }
+            for (int i = 0; i < liste.GetLength(0) ; i++)
+            {
+                for (int j = 0; j < liste.GetLength(1); j++)
+                {
+                    Console.Write(liste[i,j]+ " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+        }
+    }
+}
